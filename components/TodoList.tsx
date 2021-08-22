60510dfbdb5021090c0344e2a75b8ac94118e84a
donghoonlee94 <94donghoon.lee@gmail.com>
2021-08-22 14:21:02 +0900

    Add color nums

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -11,18 +11,119 @@ interface IProps {
 const Container = styled.div`
   width: 100%;
 
+  .todo-num {
+    margin-left: 12px;
+  }
+
   .todo-list-header {
     padding: 12px;
-    border-bottom: 1px soild ${palette.gray};
+    position: relative;
+    border-bottom: 1px solid ${palette.gray};
 
     .todo-list-last-todo {
       font-size: 14px;
+      margin: 0 0 8px;
       span {
-        margin-left: 8px;
+        margin-left: 12px;
+      }
+    }
+
+    .todo-list-header-colors {
+      display: flex;
+      .todo-list-header-color-num {
+        display: flex;
+        margin-right: 8px;
+        p {
+          font-size: 14px;
+          line-height: 16px;
+          margin: 0;
+          margin-left: 6px;
+        }
+        .todo-list-header-round-color {
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+        }
       }
     }
   }
-`
+  .bg-blue {
+    background-color: ${palette.blue};
+  }
+  .bg-green {
+    background-color: ${palette.green};
+  }
+  .bg-navy {
+    background-color: ${palette.navy};
+  }
+  .bg-orange {
+    background-color: ${palette.orange};
+  }
+  .bg-red {
+    background-color: ${palette.red};
+  }
+  .bg-yellow {
+    background-color: ${palette.yellow};
+  }
+
+  .todo-list {
+    .todo-item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      height: 52px;
+      border-bottom: 1px solid ${palette.gray};
+
+      .todo-left-side {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        .todo-color-block {
+          width: 12px;
+          height: 100%;
+        }
+        .checked-todo-text {
+          color: ${palette.gray};
+          text-decoration: line-through;
+        }
+        .todo-text {
+          margin-left: 12px;
+          font-size: 16px;
+        }
+      }
+      .todo-right-side {
+        display: flex;
+        align-items: center;
+        margin-right: 12px;
+        svg {
+          &:first-child {
+            margin-right: 16px;
+          }
+        }
+        .todo-trash-can {
+          width: 16px;
+          path {
+            fill: ${palette.deep_red};
+          }
+        }
+        .todo-check-mark {
+          fill: ${palette.deep_green};
+        }
+        .todo-button {
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          border: 1px solid ${palette.gray};
+          background-color: transparent;
+          outline: none;
+        }
+      }
+    }
+  }
+`;
+
 
 const TodoList: React.FC<IProps> = ({ todos }) => {
   const getTodoColorNums = useCallback(() => {
@@ -67,16 +168,40 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
     };
   }, [todos]);
 
-  // todos가 변경될 때만 함수와 변수를 재연산함
+  type ObjectIndexType = {
+    [key: string]: number | undefined;
+  };
 
-  const todoColorNums = useMemo(getTodoColorNums, [todos]);
-  
+  //* 색깔 객체 구하기 2
+  const todoColorNums = useMemo(() => {
+    const colors: ObjectIndexType = {};
+    todos.forEach((todo) => {
+      const value = colors[todo.color];
+      if (!value) {
+        //* 존재하지않던 key라면
+        colors[`${todo.color}`] = 1;
+      } else {
+        //* 존재하는 키라면
+        colors[`${todo.color}`] = value + 1;
+      }
+    });
+    return colors;
+  }, [todos]);  
+
   return (
     <Container>
       <div className="todo-list-header">
         <p className="todo-list-last-todo">
           남은 TODO <span>{todos.length}개</span>
         </p>
+        <div className="todo-list-header-colors">
+          {Object.keys(todoColorNums).map((color, idx) => (
+            <div className="todo-list-header-color-num" key={idx}>
+              <div className={`todo-list-header-round-color bg-${color}`} />
+              <p>{todoColorNums[color]}개</p>
+            </div>
+          ))}
+        </div>
       </div>
     </Container>
   );
